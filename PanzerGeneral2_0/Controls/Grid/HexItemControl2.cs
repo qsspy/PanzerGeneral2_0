5f4a64b9ddea02e7a0a3852115d5e0ada6d7eeec
Igor Sokół <asus.11@tlen.pl>
2020-12-26 19:49:34 +0100

    Dodano pierwszą wersję nakładania Unit na HexGrid

---
+++ PanzerGeneral2_0/Controls/Grid/HexItemControl2.cs
@@ -0,0 +1,55 @@
+﻿using HexGridControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PanzerGeneral2_0.Controls.Grid
+{
+    /// <summary>
+    /// Wykonaj kroki 1a lub 1b, a następnie 2, aby użyć tej kontrolki niestandardowej w pliku XAML.
+    ///
+    /// Krok 1a) Użycie tej kontrolki niestandardowej w pliku XAML, który istnieje w bieżącym projekcie.
+    /// Dodaj ten atrybut XmlNamespace do głównego elementu pliku znaczników, gdzie jest 
+    /// do użycia:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:PanzerGeneral2_0.Controls.Grid"
+    ///
+    ///
+    /// Krok 1b) Użycie tej kontrolki niestandardowej w pliku XAML, który istnieje w innym projekcie.
+    /// Dodaj ten atrybut XmlNamespace do głównego elementu pliku znaczników, gdzie jest 
+    /// do użycia:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:PanzerGeneral2_0.Controls.Grid;assembly=PanzerGeneral2_0.Controls.Grid"
+    ///
+    /// Należy również dodać odwołanie do projektu z projektu, w którym znajduje się plik XAML
+    /// do tego projektu i skompiluj ponownie, aby uniknąć błędów kompilacji:
+    ///
+    ///     Kliknij prawym przyciskiem myszy docelowy projekt w Eksploratorze rozwiązań i
+    ///     „Dodaj odwołanie”->„Projekty”->[Wyszukaj i wybierz ten projekt]
+    ///
+    ///
+    /// Krok 2)
+    /// Przejdź dalej i użyj swojego formantu w pliku XAML.
+    ///
+    ///     <MyNamespace:HexItemControl2/>
+    ///
+    /// </summary>
+    public class HexItemControl2 : HexItem
+    {
+        static HexItemControl2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HexItemControl2), new FrameworkPropertyMetadata(typeof(HexItemControl2)));
+        }
+    }
+}
