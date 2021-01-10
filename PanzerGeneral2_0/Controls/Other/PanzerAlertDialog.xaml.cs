0f6c5db94dc40fffab5ecaff711ff06f8b2fbc33
Daniel <danel.filipek@gmail.com>
2021-01-10 01:24:16 +0100

    Zaimplementowano customowe Dialog'i do obsługi przycisków.

---
+++ PanzerGeneral2_0/Controls/Other/PanzerAlertDialog.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PanzerGeneral2_0.Controls.Other
+{
+    /// <summary>
+    /// Interaction logic for PanzerAlertDialog.xaml
+    /// </summary>
+    public partial class PanzerAlertDialog : UserControl
+    {
+
+        public class Builder
+        {
+            public string Message { get; set; }
+
+            public Button positiveButton;
+            public Button negativeButton;
+
+            public Builder SetMessage(string message)
+            {
+                Message = message;
+                return this;
+            }
+
+            public Builder setOnPositiveClickButtonListener(string text, Action<object,EventArgs> listener)
+            {
+                positiveButton = new Button();
+                positiveButton.Content = text;
+                positiveButton.Padding = new Thickness(10, 0, 10, 0);
+                positiveButton.Margin = new Thickness(20, 0, 20, 0);
+                positiveButton.Click += new RoutedEventHandler(listener);
+                return this;
+            }
+
+            public Builder setOnNegativeClickButtonListener(string text, Action<object, EventArgs> listener)
+            {
+                negativeButton = new Button();
+                negativeButton.Content = text;
+                negativeButton.Padding = new Thickness(10, 0, 10, 0);
+                negativeButton.Margin = new Thickness(20, 0, 20, 0);
+                negativeButton.Click += new RoutedEventHandler(listener);
+                return this;
+            }
+
+            public PanzerAlertDialog create()
+            {
+                return new PanzerAlertDialog(this);
+            }
+
+            public interface OnPositiveButtonClick {
+
+                void execute(object sender, EventArgs e);
+            }
+
+            public interface OnNegativeButtonClick
+            {
+                void execute(object sender, EventArgs e);
+            }
+        }
+
+        private PanzerAlertDialog(Builder builder)
+        {
+            DataContext = builder;
+            InitializeComponent();
+
+            if(builder.positiveButton != null)
+            {
+                ButtonPanel.Children.Add(builder.positiveButton);
+            }
+            if (builder.negativeButton != null)
+            {
+               ButtonPanel.Children.Add(builder.negativeButton);
+            }
+        }
+
+        public void attachToPanel(Panel panel)
+        {
+            panel.Children.Add(this);
+        }
+    }
+}
---
+++ PanzerGeneral2_0/Controls/Other/PanzerLoadingDialogControl.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PanzerGeneral2_0.Controls.Other
+{
+    /// <summary>
+    /// Interaction logic for PanzerLoadingDialogControl.xaml
+    /// </summary>
+    public partial class PanzerLoadingDialogControl : UserControl
+    {
+
+        public class Builder
+        {
+            private PanzerLoadingDialogControl _panzerLoadingDialog;
+
+            public Builder()
+            {
+                _panzerLoadingDialog = new PanzerLoadingDialogControl();
+            }
+
+            public Builder setWaitingMessage(string message)
+            {
+                _panzerLoadingDialog.WaitingMessage = message;
+                return this;
+            }
+
+            public Builder setFinishLoadingMessage(string message)
+            {
+                _panzerLoadingDialog.FinishLoadingMessage = message;
+                return this;
+            }
+
+            public Builder setSimulatedWaitTime(int timeMilis)
+            {
+                _panzerLoadingDialog.SimulatedWaitTime = timeMilis;
+                return this;
+            }
+
+            public Builder setConfirmButtonText(string text)
+            {
+                _panzerLoadingDialog.ConfirmButtonText = text;
+                return this;
+            }
+
+            public Builder setOnLoadingListener(Action listener)
+            {
+                _panzerLoadingDialog.OnLoadingListener = listener;
+                return this;
+            }
+
+            public Builder setOnFinishButtonClickListener(Action<object,EventArgs> listener)
+            {
+                _panzerLoadingDialog.OnFinishButtonClickListener = listener;
+                return this;
+            }
+
+            public PanzerLoadingDialogControl create()
+            {
+                _panzerLoadingDialog.initailize();
+                return _panzerLoadingDialog;
+            }
+        }
+
+
+        private int _dotCount = 1;
+        private int _dotCountEditInterval = 500;
+        public int SimulatedWaitTime { get; set; } = 0;
+        public string WaitingMessage { get; set; } = "Loading";
+        public string FinishLoadingMessage { get; set; } = "Loading Finished!";
+        public string ConfirmButtonText { get; set; } = "OK";
+        public Action OnLoadingListener { get; set; } = null;
+        public Action<object, EventArgs> OnFinishButtonClickListener { get; set; } = null;
+
+
+        private PanzerLoadingDialogControl() {}
+
+        private void initailize()
+        {
+            InitializeComponent();
+            Message.Text = WaitingMessage;
+            FinishButton.Content = ConfirmButtonText;
+            if(OnFinishButtonClickListener != null)
+            {
+                FinishButton.Click += new RoutedEventHandler(OnFinishButtonClickListener);
+            }
+        }
+
+
+
+        public void startLoading()
+        {
+            Task.Run(executeTask);
+        }
+
+        private async void executeTask()
+        {
+            long timeElapsed = 0;
+
+            //simulated wait
+
+            while(timeElapsed < SimulatedWaitTime)
+            {
+                string newWaitingMessage = WaitingMessage;
+                foreach(var i in Enumerable.Range(0, _dotCount))
+                {
+                    newWaitingMessage += ".";
+                }
+                _dotCount = _dotCount % 3 + 1;
+                Application.Current.Dispatcher.Invoke(new Action(() => { Message.Text = newWaitingMessage; }));
+                timeElapsed += _dotCountEditInterval;
+                await Task.Delay(_dotCountEditInterval);
+            }
+
+            //target async operation
+
+            OnLoadingListener?.Invoke();
+
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                Message.Text = FinishLoadingMessage;
+                FinishButton.Visibility = Visibility.Visible;
+            }));
+
+        }
+
+        public PanzerLoadingDialogControl attachToPanel(Panel panel)
+        {
+            panel.Children.Add(this);
+            return this;
+        }
+    }
+}
